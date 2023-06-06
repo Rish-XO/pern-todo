73bbe56c095978ba0cd759e19955c446a61d9954
Rishal <Rishal.mhd009@gmail.com>
2023-06-07 02:53:21 +0530

    post done

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -3,26 +3,31 @@ import React, { Fragment, useState } from "react";
 function InputTodo() {
   const [description, setDescription] = useState("");
 
-const onSubmitForm = async(e) => {
-    e.preventDefault()
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
     try {
-        const body = {description}
-      
+      const body = { description };
+      const response = await fetch("http://localhost:5000/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      console.log(response);
     } catch (error) {
-        console.log(error.message);
+      console.log(error.message);
     }
-}
+  };
 
   return (
     <Fragment>
       <h1 className="text-center mt-5">Todo List</h1>
       <div className="container ">
-        <form className="d-flex mt-5">
+        <form className="d-flex mt-5" onSubmit={onSubmitForm}>
           <input
             type="text"
             className="form-control mx-5"
-            value={description} 
-            onChange={e => setDescription(e.target.value)}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
           />
           <button className="btn btn-success">Add</button>
         </form>
